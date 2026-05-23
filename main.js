561b3116d2408ae756aa9a4029706d4140386b28
Devolas <darian.dihal@gmail.com>
2026-05-22 20:32:50 -0400

    added checks for diagonal coordinates, out of range coordinates, new variable storing all ship coordinates, new tests, fixed shiplength

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ export function ship() {
     var timesHit = 0;
     var shipSunk = false;
     var shipPosition = [];
+    var shipTitle = "";
+    var shipSpaceCoordinates = [];
 
     function hit() {
         timesHit += 1;
@@ -17,19 +19,80 @@ export function ship() {
     }
 
     function setShipPos(tip, end) { // expect this format [x,y], [x,y]
+        const evaluateArray = [tip[0], tip[1], end[0], end[1]];
+        // console.log(evaluateArray);
+        if (tip[0] !== end[0] && tip[1] !== end[1]){
+            // console.log(tip[0], end[0], tip[1], end[1]);
+            throw new Error ("invalid coordinates : diagonal");
+        }
+        for (let i = 0; i < evaluateArray.length; i++) {
+            // console.log(evaluateArray[i]);
+            if (evaluateArray[i] < 0 || evaluateArray[i] > 10) { // if the coordinates are OUTSIDE of the range
+                throw new Error("invalid coordinates : out of map range");
+            }
+        }
         shipPosition = [tip, end]; // [[x,y], [x,y]]
+        calcShipLength(); // automatically calc the length so we don't forget
         return shipPosition;
     }
 
-    function getShipPos(){
+    function getShipPos() {
         return shipPosition;
     }
 
-    return { shipLength, timesHit, shipSunk, hit, isSunk, setShipPos };
+    function calcShipLength() {
+        if ((shipPosition[0][0] - shipPosition[1][0]) === 0) {
+            shipLength = Math.abs(shipPosition[0][1] - shipPosition[1][1]) + 1; // Y
+            calcShipSpace("Y");
+        } else {
+            shipLength = Math.abs(shipPosition[0][0] - shipPosition[1][0]) + 1; // X
+            calcShipSpace("X");
+        }
+    }
+
+    function calcShipSpace(axis) {
+        if (axis === "Y") {
+            const shipStaticAxis = shipPosition[0][0]; // doesn't matter if its the tip or end because it is the same
+            if (shipPosition[0][1] > shipPosition[1][1]) {
+                // shipSpaceCoordinates.push([shipStaticAxis, shipPosition[1][1]])
+                for (let i = 0; i < shipLength; i++) {
+                    shipSpaceCoordinates.push([shipStaticAxis, shipPosition[1][1] + i]);
+                }
+            } else {
+                // shipSpaceCoordinates.push([shipStaticAxis, shipPosition[0][1]])
+                for (let i = 0; i < shipLength; i++) {
+                    shipSpaceCoordinates.push([shipStaticAxis, shipPosition[0][1] + i]);
+                }
+            }
+        } else if (axis === "X") {
+            const shipStaticAxis = shipPosition[0][1]; // doesn't matter if its the tip or end because it is the same
+            if (shipPosition[0][0] > shipPosition[1][0]) {
+                for (let i = 0; i < shipLength; i++) {
+                    shipSpaceCoordinates.push([shipStaticAxis, shipPosition[0][0] + i]);
+                }
+            } else {
+                for (let i = 0; i < shipLength; i++) {
+                    shipSpaceCoordinates.push([shipStaticAxis, shipPosition[1][0] + i]);
+                }
+            }
+        }
+        return shipSpaceCoordinates;
+    }
+
+    function getShipSpaceCoordinates(){
+        console.log(shipSpaceCoordinates);
+        return shipSpaceCoordinates;
+    }
+
+    function getShipLength() {
+        return shipLength;
+    }
+
+    return { shipLength, timesHit, shipSunk, hit, isSunk, setShipPos, getShipLength, getShipPos, calcShipLength, getShipSpaceCoordinates };
 }
 
 function gameboard() {
-
+    var shipList = [];
 }
 
 
--- main.test.js
+++ main.test.js
@@ -11,9 +11,51 @@ test("ship status returns false", () => {
     expect(newShip.isSunk()).toBe(false);
 });
 
-test("ship position return [[1,2],[3,4]]", () => {
+test("ship position return [[5,2],[5,4]]", () => {
     const newShip = ship();
-    expect(newShip.setShipPos([1,2],[3,4])).toEqual([[1,2],[3,4]]);
+    expect(newShip.setShipPos([5, 2], [5, 4])).toEqual([[5, 2], [5, 4]]);
+});
+
+test("ship space coordinates return [1,2],[1,3],[1,4]", () => {
+    const newShip = ship();
+    newShip.setShipPos([1, 2], [1, 4]);
+    expect(newShip.getShipSpaceCoordinates()).toEqual([[1, 2], [1, 3], [1, 4]]);
+});
+
+test("ship position throw error above range", () => {
+    const newShip = ship();
+    expect(() => {
+        (newShip.setShipPos([11, 2], [3, 2])); npx
+    }).toThrow("invalid coordinates : out of map range");
+});
+
+test("ship position throw error below range", () => {
+    const newShip = ship();
+    expect(() => {
+        (newShip.setShipPos([10, 2], [10, -4])); npx
+    }).toThrow();
+});
+
+test("ship position throw error diagonal coordinates", () => {
+    const newShip = ship();
+    expect(() => {
+        (newShip.setShipPos([1, 2], [3, 4]));
+    }).toThrow("invalid coordinates : diagonal");
+});
+
+
+test("ship length return 3", () => {
+    const newShip = ship();
+    newShip.setShipPos([3, 2], [3, 4]);
+    newShip.calcShipLength();
+    expect(newShip.getShipLength()).toBe(3);
+});
+
+test("ship length return 5", () => {
+    const newShip = ship();
+    newShip.setShipPos([5, 2], [1, 2]);
+    newShip.calcShipLength();
+    expect(newShip.getShipLength()).toBe(5);
 });
 
 
